6d7b448258de1aa0f0b685ae0c78f843d978e71a
Rustam Ismagilov <rustam.ismagilove@gmail.com>
2023-03-31 17:43:17 +0300

    Minor changes

--- Assets/Scripts/ScoreCounter.cs
+++ Assets/Scripts/ScoreCounter.cs
@@ -3,29 +3,29 @@ using TMPro;
 
 public class ScoreCounter : MonoBehaviour
 {
-    public TextMeshProUGUI scoreText;  // A reference to the TextMeshProUGUI component in the scene that will display the score
-    private int scorePoints = 0;        // The default score value
+    public TextMeshProUGUI scoreText;
+    private int scorePoints = 0;
+    private ScoreMultiplier scoreMultiplier;
 
     public void Start()
     {
-        UpdateScoreText();  // Updates the score text in the UI when the script is first started
+        scoreMultiplier = FindObjectOfType<ScoreMultiplier>(); // Find the ScoreMultiplier component in the scene
+        UpdateScoreText();
     }
 
-    // Update the score displayed in the UI based on the current value of the scorePoints value
     public void UpdateScoreText()
     {
-        scoreText.text = "Score: " + GetScore();   // Set the text of the scoreText component to the current score
+        scoreText.text = "Score: " + GetScore();
     }
 
-    // This method adds scorePoints to the overall amount of scorePoints
     public void AddPoints(int newPoints)
     {
-        scorePoints += newPoints;   // Add new points to the current score
+            scorePoints += newPoints * scoreMultiplier.scoreMultiplier;
+            UpdateScoreText();   
     }
 
-    // This method returns the current score
     public int GetScore()
     {
-        return scorePoints; // Return the current score value
+        return scorePoints;
     }
 }
